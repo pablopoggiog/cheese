15bfa82a625ebdb60c86e3651b169b34f5d0f8c4
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-04 19:13:07 -0300

    Create types directory

---
+++ types/Types.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+library Types {
+    struct Item {
+        string name;
+        string description;
+        string image;
+        string variant;
+    }
+}
