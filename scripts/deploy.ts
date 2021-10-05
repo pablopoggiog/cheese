bc081e60510d605d7cf405e3538ae87937950cac
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-05 00:49:38 -0300

    Remove boilerplate in deploy.ts and configure it

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,21 @@
-import { ethers, run } from 'hardhat';
+import { ethers, run } from "hardhat";
 
 async function main() {
-  const Greeter = await ethers.getContractFactory('Greeter');
-  const greeter = await Greeter.deploy('Hello, Hardhat!');
+  const nftContractFactory = await ethers.getContractFactory("NonFungibleCheese");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
 
-  await greeter.deployed();
-
-  console.log('Greeter deployed to:', greeter.address);
-
-  // wait until the contract is available across the entire net
-  await new Promise((resolve) => setTimeout(resolve, 1000 * 30));
-
-  await run('verify:verify', {
-    address: greeter.address,
-    constructorArguments: ['Hello, Hardhat!'],
-  });
+  console.log("Contract deployed to:", nftContract.address);
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
     process.exit(1);
-  });
+  }
+};
+
+runMain();
